b24f3b4b216cfdf964ef8c1b823c5ace74acb834
greg <gregt.dev@outlook.com>
2026-07-31 13:37:00 +0100

    Add debug logs

--- cli/internal/transport/client.go
+++ cli/internal/transport/client.go
@@ -3,6 +3,7 @@ package transport
 import (
 	"errors"
 	"fmt"
+	"log/slog"
 	"net"
 	"os"
 	"strconv"
@@ -37,6 +38,7 @@ func (client *Client) Init() error {
 }
 
 func (client *Client) SendRequestMessage(kind MessageKind, payload string) error {
+	slog.Debug("Sending request message")
 	var message Message = Message{"ip", "external", Request, kind, payload}
 	return client.sendMessage(message)
 }
@@ -81,6 +83,7 @@ func (client *Client) sendMessage(message Message) error {
 	if err != nil {
 		return err
 	}
+	slog.Debug("Found address of an active node", "address", addr)
 
 	resolvedAddr, err := net.ResolveUDPAddr("udp", addr.String())
 	if err != nil {
@@ -92,6 +95,7 @@ func (client *Client) sendMessage(message Message) error {
 		return err
 	}
 	defer conn.Close()
+	slog.Debug("Set up UDP socket", "address", addr)
 
 	messageBytes, err := messageToBytes(message)
 	if err != nil {
@@ -99,6 +103,9 @@ func (client *Client) sendMessage(message Message) error {
 	}
 
 	_, err = conn.Write(messageBytes)
+	if err == nil {
+		slog.Debug("Wrote bytes to UDP socket", "byte count", len(messageBytes))
+	}
 	return err
 }
 
@@ -110,17 +117,20 @@ func (client *Client) getActiveNodeAddress() (net.IP, error) {
 		if err != nil {
 			return net.IP{}, err
 		}
+		slog.Debug("Found ips", "ip count", len(ips))
 
 		for _, ip := range ips {
 			err = client.SendRequestMessage(Ping, "")
 			if err != nil {
 				return net.IP{}, err
 			}
+			slog.Debug("Sent ping request")
 
 			_, err = client.GetResponseMessage(Ping)
 			if err == nil {
 				return ip, nil
 			}
+			slog.Debug("Received ping response")
 		}
 	}
 
--- cli/main.go
+++ cli/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"chaos-dragon/cli/internal/commands"
 	"fmt"
+	"log/slog"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -12,15 +14,26 @@ func main() {
 		fmt.Println("Usage: chaos-dragon [-s something]")
 		return
 	}
+	slog.Debug("Parsed valid args")
+
+	logLevel := getLogLevel()
+	options := &slog.HandlerOptions{Level: logLevel}
+	handler := slog.NewTextHandler(os.Stdout, options)
+	slog.SetDefault(slog.New(handler))
+	slog.Debug("Configured logger", "level", logLevel)
 
 	switch args[0] {
 	case "ping":
+		slog.Debug("Running ping command")
 		commands.Ping()
 	case "log":
+		slog.Debug("Running log command")
 		commands.Log()
 	case "scan":
+		slog.Debug("Running scan command")
 		commands.Scan()
 	case "broadcast":
+		slog.Debug("Running broadcast command")
 		commands.Broadcast()
 	default:
 		fmt.Printf("Unknown command: %s\n", args[0])
@@ -28,4 +41,23 @@ func main() {
 	}
 
 	os.Exit(0)
+}
+
+func getLogLevel() slog.Level {
+	logLevel := os.Getenv("LOG_LEVEL")
+
+	switch strings.ToLower(logLevel) {
+	case "trace":
+		return slog.LevelDebug
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelInfo
+	}
 }
